7edb1aecf73222e13943cdc0ab7a5285a1f7aec6
William Young <wadyoung@outlook.com>
2021-07-02 11:02:01 +0100

    Added roll output

--- spec/dice_game_spec.rb
+++ spec/dice_game_spec.rb
@@ -4,7 +4,12 @@ describe Dice do
   it "Intialises a dice object" do
   expect(Dice.new).to be_instance_of(Dice)
   end
+  
   it "should roll a dice" do
   expect(subject).to respond_to(:roll)
+  end
+  
+  it "Should output between 1 and 6" do
+  expect(subject.roll).to eq(5)
   end
 end
