bfca97129e2e5bf7871a5794f76e816b7b81fa1d
William Young <wadyoung@outlook.com>
2021-07-02 11:34:34 +0100

    Added multiple dice rolls

--- lib/dice_game.rb
+++ lib/dice_game.rb
@@ -1,5 +1,5 @@
 class Dice
- def roll()
-  rand(1..6)
+ def roll(num = 1)
+   num.times { p rand(1..6) }
  end
 end
--- spec/dice_game_spec.rb
+++ spec/dice_game_spec.rb
@@ -1,15 +1,20 @@
 require "dice_game"
 
 describe Dice do
-  it "Intialises a dice object" do
+  it "Intialises a die object" do
   expect(Dice.new).to be_instance_of(Dice)
   end
   
-  it "should roll a dice" do
+  it "should roll a die" do
   expect(subject).to respond_to(:roll)
   end
   
   it "Should output randomly between 1 and 6" do
   expect(subject.roll).to be_between(1, 6)
   end
+
+  it "Should be able to roll multiple die" do
+  expect(subject).to respond_to(:roll).with(1).argument
+  end
+
 end
