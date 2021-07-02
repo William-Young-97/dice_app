91900dfd12ee219fe56bbf79bf8157a9bdf49a15
jamescondon <jecondon66@icloud.com>
2021-07-02 10:39:41 +0100

    Added roll method

--- spec/dice_game_spec.rb
+++ spec/dice_game_spec.rb
@@ -1,7 +1,10 @@
-require dice_game
+require "dice_game"
 
 describe Dice do
   it "Intialises a dice object" do
   expect(Dice.new).to be_instance_of(Dice)
+  end
+  it "should roll a dice" do
+  expect(subject).to respond_to(:roll)
   end
 end
